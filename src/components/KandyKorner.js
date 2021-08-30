12b26c6cc5dd96afde1a8bce862700c2c69ad68b
CassieTesauro <cassie.tesauro@gmail.com>
2021-08-30 18:20:11 -0500

    working on rendering product properties

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,5 +1,6 @@
 import React from "react" //  Allows us to use React library features
 import { LocationList } from "./locations/LocationList" //for LocationList function
+import { ProductList } from "./products/ProductsList"
 
 //Module Responsibility: One function that determines the layout of the site
 
@@ -8,8 +9,11 @@ export const KandyKorner = () => {
         <>
             <h1>Kandy Korner</h1>
 
-            <h2>Store Locations</h2>
+            <h2>Store Lokations</h2>
             <LocationList />
+
+            <h2>Our Kandies</h2>
+            <ProductList />
         </>
     )
 }
---
+++ src/components/products/ProductsList.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react"  //import react and built-in hooks
+
+//Module Responsibility: house a function that handles the products data.  store, listen/react, render
+
+
+export const ProductList = () => {
+    const [products, productsFunction] = useState([]) //prepare to store the data locally.  state is initially an empty array.  
+
+
+    useEffect( //initial useEffect to fetch the data and store it 
+        () => {
+            fetch("http://localhost:8088/products") //fetch and translate the API JSON state
+                .then(res => res.json())
+                .then((fetchedAPIProductsData) => { 
+                    productsFunction(fetchedAPIProductsData) //use the function to put the state into the locations variable to store locally
+                })
+            },
+        []
+    )
+
+    return ( //code in the curly brackets is JS
+        <>
+            {
+                products.map(
+                    (product) => {
+                        return <div key={`product--${product.id}`}>
+                                    <ul>{product.name}
+                                        <li>{product.id}</li>
+                                        <li>{product.price}</li>
+                                        <li>{product.productTypeId}</li>
+                                    </ul>
+                               </div>
+                    }
+                )
+            }
+        </>
+    )
+
+
+
+}
