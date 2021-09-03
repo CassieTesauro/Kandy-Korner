e34c9c46b93b2a3ec1aa13e27b7d5a971de16086
CassieTesauro <cassie.tesauro@gmail.com>
2021-09-03 11:06:03 -0500

    manager checkbox on employee form is working

--- src/components/employees/EmployeeForm.js
+++ src/components/employees/EmployeeForm.js
@@ -6,16 +6,22 @@ export const EmployeeForm = () => {
     const [employee, updateEmployee] = useState({
         name: "",
         hourlyRate: 0,
+        manager: false,
+        fullTime: false
+
     });
 
     const history = useHistory()
 
     const finishHiring = (evt) => {
         evt.preventDefault()
-        
+
         const newEmployee = {
             name: employee.name,
-            hourlyRate: employee.hourlyRate
+            hourlyRate: employee.hourlyRate,
+            manager: employee.manager,
+            fullTime: employee.fullTime
+
         }
 
         const fetchOptions = {
@@ -27,54 +33,68 @@ export const EmployeeForm = () => {
         }
 
         return fetch("http://localhost:8088/employees", fetchOptions)
-        .then(() => {
-            history.push("/employees")
-        })
+            .then(() => {
+                history.push("/employees")
+            })
 
-}
+    }
 
-return (
-    <form className="employeeForm">
-        <h2 className="employeeForm__title">New Employee</h2>
-        <fieldset>
-            <div className="form-group">
-                <label htmlFor="name">Name:</label>
-                <input
-                    onChange={
-                        (evt) => {
-                            const copy = { ...employee }
-                            copy.name = evt.target.value
-                            updateEmployee(copy)
+    return (
+        <form className="employeeForm">
+            <h2 className="employeeForm__title">New Employee</h2>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="name">Name:</label>
+                    <input
+                        onChange={
+                            (evt) => {
+                                const copy = { ...employee }
+                                copy.name = evt.target.value
+                                updateEmployee(copy)
+                            }
                         }
-                    }
-                    required autoFocus
-                    type="text"
-                    className="form-control"
-                    placeholder="Full Name"
-                />
-            </div>
-        </fieldset>
-        <fieldset>
-            <div className="form-group">
-                <label htmlFor="hourlyRate">Hourly Rate:</label>
-                <input
-                    onChange={
-                        (evt) => {
-                            const copy = { ...employee }
-                            copy.hourlyRate = evt.target.value
-                            updateEmployee(copy)
+                        required autoFocus
+                        type="text"
+                        className="form-control"
+                        placeholder="Full Name"
+                    />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="hourlyRate">Hourly Rate:</label>
+                    <input
+                        onChange={
+                            (evt) => {
+                                const copy = { ...employee }
+                                copy.hourlyRate = evt.target.value
+                                updateEmployee(copy)
+                            }
                         }
-                    }
-                    required autoFocus
-                    type="text"
-                    className="form-control"
-                    placeholder="Hourly Rate Here"
-                />
-            </div>
-        </fieldset>
-        <button className="btn btn-primary" onClick={finishHiring}>
-            Finish Hiring
-        </button>
-    </form>
-)
+                        required autoFocus
+                        type="text"
+                        className="form-control"
+                        placeholder="Hourly Rate Here"
+                    />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="manager">Manager:</label>
+                    <input
+                        onChange={
+                            (evt) => {
+                                const copy = { ...employee }
+                                copy.manager = evt.target.checked
+                                updateEmployee(copy)
+                            }
+                        }
+                        type="checkbox" />
+                </div>
+            </fieldset>
+            <button className="btn btn-primary" onClick={finishHiring}>
+                Finish Hiring
+            </button>
+        </form>
+    )
 }
